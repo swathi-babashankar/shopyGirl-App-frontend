2319d9795d580cdf187602fefe0858de57de41d4
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2024-12-20 09:16:41 +0530

    Update AddProducts.jsx

--- components/adminComponents/AddProducts.jsx
+++ components/adminComponents/AddProducts.jsx
@@ -51,7 +51,7 @@ export default function AddProduct(){
 
         try{
 
-            const response = await axios.post(`http://localhost:4000/createProduct?adminId=${adminId}`,  formData, {withCredentials: true})
+            const response = await axios.post(`${import.meta.env.BACKEND_URL}/createProduct?adminId=${adminId}`,  formData, {withCredentials: true})
             console.log(response);
             dispatch(addProduct(response.data.productCreated))
             setSuccessMsg(response.data.message)
