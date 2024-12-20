de80f37dfbd207dd603ab4c0272bb93eda760614
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2024-12-20 09:24:38 +0530

    Update AllCoupons.jsx

--- components/adminComponents/AllCoupons.jsx
+++ components/adminComponents/AllCoupons.jsx
@@ -23,7 +23,7 @@ export default function AllCoupons({couponCode, discount, validTill, _id}){
 
         try {
 
-            const response = await axios.delete(`http://localhost:4000/deleteCoupon/?couponId=${_id}&adminId=${adminId}`, {withCredentials: true});
+            const response = await axios.delete(`${import.meta.env.BACKEND_URL}/deleteCoupon/?couponId=${_id}&adminId=${adminId}`, {withCredentials: true});
             dispatch(deleteCoupon(response.data.couponDeleted._id));
 
             location.reload();
