f8fd0b821b9d34cbb4477cba04c8424c42193eb4
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-09-05 15:03:18 +0530

    Update userAuthSlice.js

--- features/user/userAuthSlice.js
+++ features/user/userAuthSlice.js
@@ -13,14 +13,13 @@ export const userAuthSlice = createSlice({
     reducers: {
 
         login: (state, action) => {
-            // state.status = true
-            // state.userData = action.payload
-            ...state,
-            userData: action.payload,
+            state.status = true
+            state.userData = action.payload
+            
             // state.userData.email = action.payload.email
             // state.userData.password = action.payload.password
 
-            action.type = "USER_LOGGEDIN_SUCCESSFULLY"
+            // action.type = "USER_LOGGEDIN_SUCCESSFULLY"
         },
 
         logout: (state, action) => {
@@ -37,3 +36,4 @@ export const userAuthSlice = createSlice({
 export const {login, logout} = userAuthSlice.actions;
 
 export default userAuthSlice.reducer;
+
