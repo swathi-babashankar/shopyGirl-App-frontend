fb12dc31889b3b5b479c2ed9c02b4c5ec158a657
Swathi <114010937+swathi-babashankar@users.noreply.github.com>
2025-10-09 09:16:43 +0530

    Update Order.jsx

--- components/orderComponent/Order.jsx
+++ components/orderComponent/Order.jsx
@@ -75,7 +75,7 @@ export default function Order() {
 
         if(userId) {
   
-        const response = await axios.get(`${import.meta.env.BACKEND_URL}/getAllOrders/?userId=${userId}`, { withCredentials: true });
+        const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/getAllOrders/?userId=${userId}`, { withCredentials: true });
         console.log("getOrders", response);
         setSavedAddress(response.data.allOrders[0]);
         }
@@ -100,7 +100,7 @@ export default function Order() {
         if(id){
           console.log("try");
 
-          const response = await axios.get(`${import.meta.env.BACKEND_URL}/getProductById/?prodId=${id}`);
+          const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/getProductById/?prodId=${id}`);
           console.log(response);
           setOrderingProduct(response?.data.getProdById);
           
@@ -139,7 +139,7 @@ export default function Order() {
       if(id) {
         console.log("productid", id);
 
-        const response = await axios.post(`${import.meta.env.BACKEND_URL}/orderSingleProduct/?prodId=${id}&userId=${userId}`, data, { withCredentials: true } );
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/orderSingleProduct/?prodId=${id}&userId=${userId}`, data, { withCredentials: true } );
         console.log(response);
   
         setSuccessMsg(response.data.message);
@@ -147,7 +147,7 @@ export default function Order() {
 
       else {
 
-        const response = await axios.post(`${import.meta.env.BACKEND_URL}/placeOrder/?userId=${userId}`, data, {withCredentials: true});
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/placeOrder/?userId=${userId}`, data, {withCredentials: true});
         console.log("orderCartprof",response);
 
         // dispatch( placeOrder(response.data.findingProduct))
@@ -281,5 +281,6 @@ export default function Order() {
         </>
     )
 }
+
 
 
